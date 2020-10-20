c2aca0ab8227a135189067f2610f930a09a55231
njsho <noah.shoemaker@doane.edu>
2020-10-19 20:20:11 -0500

    first

---
+++ Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Home = () => {
+    
+    return(
+        <View>
+            <Text>Welcome to the Neighborhood Virtual Tour!</Text>
+        </View>
+    );
+}
+
+export default Home;
