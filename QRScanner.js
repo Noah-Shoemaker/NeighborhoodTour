ee05e96fc44b43c79a56047c084c59eb7157eec7
njsho <noah.shoemaker@doane.edu>
2020-10-19 20:34:12 -0500

    Added new screens

---
+++ QRScanner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Home = () => {
+    
+    return(
+        <View>
+            <Text>Welcome to the QR Scanner!</Text>
+        </View>
+    );
+}
+
+export default Home;
