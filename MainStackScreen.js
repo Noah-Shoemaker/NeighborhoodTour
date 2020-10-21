ea6cfc25dbe77e4e2431b33e10a45c6c188b2554
njsho <noah.shoemaker@doane.edu>
2020-10-20 20:38:23 -0500

    fixed more imports

--- MainStackScreen.js
+++ MainStackScreen.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './HomeScreen';
+import ArchiveScreen from './ArchiveScreen';
+import QRScannerScreen from './QRScannerScreen';
+import MapScreen from './MapScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -8,12 +12,12 @@ function MainStackScreen() {
 
     return (
         <NavigationContainer>
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Archive" component={ArchiveScreen}/>
-            <Tab.Screen name="QRScanner" component={QRScannerScreen} />
-            <Tab.Screen name="Map" component={MapScreen} />
-        </Tab.Navigator>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Archive" component={ArchiveScreen}/>
+                <Tab.Screen name="QRScanner" component={QRScannerScreen} />
+                <Tab.Screen name="Map" component={MapScreen} />
+            </Tab.Navigator>
         </NavigationContainer>
         
     );
