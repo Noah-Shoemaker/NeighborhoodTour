645739d039158bd9ce2ffc827733e0584f841a36
njsho <noah.shoemaker@doane.edu>
2020-10-20 20:40:16 -0500

    imports

--- MainStackScreen.js
+++ MainStackScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './HomeScreen';
 import ArchiveScreen from './ArchiveScreen';
@@ -11,14 +10,12 @@ const Tab = createBottomTabNavigator();
 function MainStackScreen() {
 
     return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Archive" component={ArchiveScreen}/>
-                <Tab.Screen name="QRScanner" component={QRScannerScreen} />
-                <Tab.Screen name="Map" component={MapScreen} />
-            </Tab.Navigator>
-        </NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Archive" component={ArchiveScreen}/>
+            <Tab.Screen name="QRScanner" component={QRScannerScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+        </Tab.Navigator>
         
     );
 }
