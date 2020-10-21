5d5afcd28c453fc7c5a95727059b6e7288883614
njsho <noah.shoemaker@doane.edu>
2020-10-20 20:52:03 -0500

    added modal button in archive

--- ArchiveScreen.js
+++ ArchiveScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const ArchiveScreen = () => {
+const ArchiveScreen = ({navigation}) => {
     
     return(
         <View>
             <Text>Welcome to the Archive!</Text>
+            <Button
+            onPress={() => navigation.navigate('Article')}
+            title="Article"
+            />
         </View>
     );
 }
