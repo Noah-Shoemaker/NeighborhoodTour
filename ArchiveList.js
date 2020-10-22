3437b2f66a5cfe847d0a80fe14dc3d59b27116f6
njsho <noah.shoemaker@doane.edu>
2020-10-21 22:54:24 -0500

    added flatlist import

--- ArchiveList.js
+++ ArchiveList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, FlatList } from 'react-native';
 
 const ArchiveList = ({navigation}) => {
     
