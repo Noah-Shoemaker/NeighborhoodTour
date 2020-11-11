d6801506b7d4cb017f49dbbbe5bfefc91f544cba
njsho <noah.shoemaker@doane.edu>
2020-11-10 21:31:33 -0600

    fixed map

--- MapScreen.js
+++ MapScreen.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import MapView from "react-native-maps";
 
-const MapScreen = () => (
-    <View>
+const MapScreen = () => {
 
-        <MapView
-            initialRegion={{
-                latitude: 40.805890,
-                longitude: -96.702722,
-                latitudeDelta: -0.014109,
-                longitudeDelta: 0.02047
-            }}
-        />
+    return(
+        <View>
 
-    </View>
-)
+            <MapView
+                style={{ flex: 1 }}
+                initialRegion={{
+                    latitude: 40.805890,
+                    longitude: -96.702722,
+                    latitudeDelta: -0.014109,
+                    longitudeDelta: 0.02047
+                }}
+            />
+
+        </View>
+    )
+}
 
 export default MapScreen;
