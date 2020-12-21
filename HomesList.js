589f6157861309cdc411695ed5ba6ebefe145c49
njsho <noah.shoemaker@doane.edu>
2020-12-21 16:20:47 -0600

    added code to render json home list in componet HomesList

--- HomesList.js
+++ HomesList.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Text, View, Button, FlatList } from 'react-native';
-import dataBase from "/DataBase.json";
+import dataBase from "./DateBase.json";
+
+const DATA = dataBase.homes;
 
 const HomesList = ({navigation}) => {
     
     return(
         <View>
             <FlatList
-                data = {dataBase.homes}
+                data = {DATA}
                 renderItem={({item}) => <Text onPress={() => navigation.navigate("Article")}>{item.name}</Text>}
             />
         </View>
