71de2e8c4265ce3b1e28cd889f2ee861914d4f22
njsho <noah.shoemaker@doane.edu>
2020-12-21 22:26:37 -0600

    added markers to map and method to open article when marker pressed

--- MapScreen.js
+++ MapScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
+import dataBase from "./DateBase.json";
 
 const MapScreen = () => {
+
+    const markers = dataBase.homes;
 
     return(
 
@@ -13,8 +16,12 @@ const MapScreen = () => {
                     longitude: -96.702722,
                     latitudeDelta: 0.014109,
                     longitudeDelta: 0.02047
-                }}
-            />
+                }}>
+
+                {markers.map((marker) => (<Marker onPress={() => navigation.navigate("Article",marker)} 
+                                                           coordinate={marker.coord} title={marker.name} />) ) }
+                
+            </MapView>
 
     );
 };
