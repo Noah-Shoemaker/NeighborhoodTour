54c71575bc8f9c1ef6d2d62cdabbb484e20f39b5
njsho <noah.shoemaker@doane.edu>
2020-12-27 17:47:09 -0600

    added error and wrong password handling for authentication

--- MainStackScreen.js
+++ MainStackScreen.js
@@ -8,6 +8,7 @@ import ArchiveStack from './ArchiveStack';
 import { TextInput } from 'react-native-gesture-handler';
 import dataBase from "./DateBase.json";
 import { useEffect } from 'react';
+import { useRef } from 'react';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,14 +16,25 @@ function MainStackScreen() {
 
     const [auth, setauth] = useState(false);
     const [value, onChangeText] = useState('');
+    const ref = useRef(auth);
 
     useEffect(() => {dataBase.authentication.authState = auth}, [auth]);
+
+    function checkPassword(e) {
+
+        setauth(e === dataBase.authentication.password);
+        ref.current = (e === dataBase.authentication.password);
+        
+        if (ref.current === false) {
+            alert("Wrong password, please try again.");
+        }
+    }
     
     if (auth === false) {
         return (
-            <View style={{flex: 1, justifyContent: 'center', alignItems:'center'}}>
+            <View style={{flex: 1, justifyContent:'center', alignItems:'center', alignContent:'center'}}>
                 <Text>Please enter the password from the booklet to access app.</Text>
-                <TextInput onSubmitEditing={event => setauth(event.nativeEvent.text === dataBase.authentication.password)}
+                <TextInput onSubmitEditing={event => checkPassword(event.nativeEvent.text)}
                   style={{ height: 40, width: 150, borderColor: 'gray', borderWidth: 1}}
                   onChangeText={text => onChangeText(text)}
                   value={value}
