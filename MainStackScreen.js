4753a062452e67193056530cbfc1fbb381bbc397
njsho <noah.shoemaker@doane.edu>
2020-12-26 20:23:41 -0600

    changed textinput box for password

--- MainStackScreen.js
+++ MainStackScreen.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './HomeScreen';
 import QRScannerScreen from './QRScannerScreen';
@@ -12,12 +13,16 @@ const Tab = createBottomTabNavigator();
 function MainStackScreen() {
 
     const [auth, setauth] = useState(false);
-
+    const [value, onChangeText] = React.useState('');
+    
     if (auth === false) {
         return (
             <View>
                 <Text>Please enter the password from the booklet to access app.</Text>
-                <TextInput onSubmitEditing={text => setauth(text === dataBase.authentication.password)}/>
+                <TextInput onSubmitEditing={text => setauth(text === dataBase.authentication.password)}
+                  style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                  onChangeText={text => onChangeText(text)}
+                  value={value}/>
             </View>
         );
     }
