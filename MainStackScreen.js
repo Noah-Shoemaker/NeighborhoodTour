a7dc64b11a48cf44893005c61f64af2611e3d4d7
njsho <noah.shoemaker@doane.edu>
2020-12-26 22:18:01 -0600

    added security to password input

--- MainStackScreen.js
+++ MainStackScreen.js
@@ -7,36 +7,45 @@ import MapScreen from './MapScreen';
 import ArchiveStack from './ArchiveStack';
 import { TextInput } from 'react-native-gesture-handler';
 import dataBase from "./DateBase.json";
+import { useEffect } from 'react';
 
 const Tab = createBottomTabNavigator();
 
 function MainStackScreen() {
 
     const [auth, setauth] = useState(false);
-    const [value, onChangeText] = React.useState('');
+    const [value, onChangeText] = useState('');
+
+    useEffect(() => {dataBase.authentication.authState = auth}, [auth]);
     
     if (auth === false) {
         return (
-            <View>
-                <Text>Please enter the password from the booklet to access app.</Text>
-                <TextInput onSubmitEditing={text => setauth(text === dataBase.authentication.password)}
+            <View style={{flex: 1}}>
+                <Text style={{flex: 1}}>Please enter the password from the booklet to access app.</Text>
+                <TextInput onSubmitEditing={event => setauth(event.nativeEvent.text === dataBase.authentication.password)}
                   style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
                   onChangeText={text => onChangeText(text)}
-                  value={value}/>
+                  value={value}
+                  textContentType='password' 
+                  secureTextEntry={true}
+                  placeholder="Enter password"
+                  style={{flex: 1}}
+                  />
             </View>
         );
     }
-    else {
-        return (
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Archive" component={ArchiveStack}/>
-                <Tab.Screen name="QRScanner" component={QRScannerScreen} />
-                <Tab.Screen name="Map" component={MapScreen} />
-            </Tab.Navigator>
+    
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Archive" component={ArchiveStack}/>
+            <Tab.Screen name="QRScanner" component={QRScannerScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+        </Tab.Navigator>
             
         );
-    }
 }
+
+
 
 export default MainStackScreen;
