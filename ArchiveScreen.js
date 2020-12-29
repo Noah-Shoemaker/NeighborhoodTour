eaaf808d333d41ef7ff6e90713343c1ba5d5412a
njsho <noah.shoemaker@doane.edu>
2020-12-28 19:29:56 -0600

    added styling to archive screen and homes list

--- ArchiveScreen.js
+++ ArchiveScreen.js
@@ -1,20 +1,59 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import HomesList from './HomesList';
 
 const ArchiveScreen = ({navigation}) => {
     
     return(
-        <View>
 
-            <Text>Welcome to the Archive!</Text>
+        <View style={styles.container}>
 
-            <Button onPress={() => navigation.navigate('Homes List')} title="Homes List" />
+              <Text style={styles.bigText}>Welcome to the Archive!</Text>
 
-            <Button onPress={() => navigation.navigate('Feature List')} title="Feature List" />
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Homes List')}>
+                <Text style={styles.text}>Homes List</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Feature List')}> 
+                <Text style={styles.text}>Feature List</Text>
+            </TouchableOpacity>
 
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 40,
+      marginBottom: 100,
+      justifyContent: 'space-between',
+      alignItems:'center',
+      alignContent:'center'
+    },
+    bigText: {
+      fontFamily: "Cochin",
+      fontWeight: 'bold',
+      textAlign: 'center',
+      width: 300,
+      fontSize: 25
+    },
+    text: {
+      fontFamily: "Cochin",
+      textAlign: 'center',
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: 'white',
+      margin: 10
+    },
+    button: {
+      backgroundColor: '#346c24',
+      borderWidth: 2,
+      borderColor: 'grey',
+      borderTopLeftRadius: 20,
+      borderTopRightRadius: 20,
+      borderBottomLeftRadius: 20,
+      borderBottomRightRadius: 20
+    }
+  });
 export default ArchiveScreen;
--- HomesList.js
+++ HomesList.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import { Text, View, Button, FlatList } from 'react-native';
+import { Text, View, Button, FlatList, StyleSheet } from 'react-native';
 import dataBase from "./DateBase.json";
 
 const HomesList = ({navigation}) => {
   
     return(
-        <View>s
+        <View style={styles.container}>
             <FlatList
                 data = {dataBase.homes}
-                renderItem={ ({item}) => (<Text onPress={() => navigation.navigate("Article", item)}> {item.name} </Text>) }
+                renderItem={ ({item}) => <Text style={styles.text} onPress={() => navigation.navigate("Article", item)}> {item.name} </Text> }
             />
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 40,
+      alignItems:'center',
+      alignContent:'center'
+    },
+    text: {
+      fontFamily: "Cochin",
+      textAlign: 'center',
+      width: 350,
+      fontSize: 20,
+      marginBottom: 10
+    }
+  });
+  
 export default HomesList;
